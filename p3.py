7c0c92f672c555afc5a71879b328c1070152472f
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2024-10-26 15:59:43 -0400

    Division HR problem

---
+++ p3.py
@@ -0,0 +1,12 @@
+# Hacker Rank Python problem sets
+# Problem 3: Division 
+# 10/26/2024
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a//b)
+    print(a/b)
+
+    
